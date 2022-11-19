e3c1b8d760d9755c94d48a4ba157f613a052ca9e
eballo <enric.ballo@gmail.com>
2022-11-19 11:38:34 +0100

    add day 02

---
+++ 2021/day02/day02.py
@@ -0,0 +1,57 @@
+from typing import Tuple
+
+
+def load_file(file: str) -> Tuple[str, int]:
+    with open(file, "r") as f:
+        for line in f:
+            direction, value = line.split()
+            yield direction, int(value)
+
+
+def part_one(file: str) -> None:
+    depth = 0
+    horizontal = 0
+
+    for direction, number in load_file(file):
+        print(f"direction {direction} - number {number}")
+        match direction:
+            case "forward":
+                horizontal = horizontal + number
+            case "down":
+                depth = depth + number
+            case "up":
+                depth = depth - number
+            case _:
+                print("something went wrong!")
+
+    print(f" horizontal {horizontal} - depth {depth}")
+    print(f" total : {horizontal * depth}")
+
+
+def part_two(file: str) -> None:
+    depth = 0
+    horizontal = 0
+    aim = 0
+
+    for direction, number in load_file(file):
+        print(f"direction {direction} - number {number}")
+        match direction:
+            case "forward":
+                horizontal = horizontal + number
+                depth = depth + (aim * number)
+            case "down":
+                aim = aim + number
+            case "up":
+                aim = aim - number
+            case _:
+                print("something went wrong!")
+
+    print(f" horizontal {horizontal} - depth {depth}")
+    print(f" total : {horizontal * depth}")
+
+
+if __name__ == '__main__':
+    # part_one("test.txt")
+    # part_one("input.txt")
+    # part_two("test.txt")
+    part_two("input.txt")
