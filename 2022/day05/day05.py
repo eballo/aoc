2ee0ef918d9c55f60a852169f885feac13f1ed6c
eballo <enric.ballo@gmail.com>
2022-12-06 10:56:16 +0100

    fix manual data input! - that's why it was not working

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -1,8 +1,9 @@
+import copy
 from typing import List, Tuple
 
 TEST_DATA = [['Z', 'N'], ['M', 'C', 'D'], ['P']]
 INPUT_DATA = [
-    ['R', 'P', 'C', 'D', 'D', 'B', 'G'],
+    ['R', 'P', 'C', 'D', 'B', 'G'],
     ['H', 'V', 'G'],
     ['N', 'S', 'Q', 'D', 'J', 'P', 'M'],
     ['P', 'S', 'L', 'G', 'D', 'C', 'N', 'M'],
@@ -31,9 +32,9 @@ def part_one(file: str):
     raw_data = load_file(file)
 
     if "test" in file:
-        data = TEST_DATA
+        data = copy.deepcopy(TEST_DATA)
     else:
-        data = INPUT_DATA
+        data = copy.deepcopy(INPUT_DATA)
 
     for line in raw_data:
 
@@ -58,10 +59,9 @@ def part_two(file: str):
     raw_data = load_file(file)
 
     if "test" in file:
-        data = TEST_DATA
+        data = copy.deepcopy(TEST_DATA)
     else:
-        data = INPUT_DATA
-
+        data = copy.deepcopy(INPUT_DATA)
     for line in raw_data:
         if "move" in line:
             # print(data)
@@ -69,21 +69,13 @@ def part_two(file: str):
             amount, from_col, to_col = get_moves(line)
             # print(f" amount {amount}, from {from_col}, to {to_col}")
             # print(data[from_col - 1])
-            temp_list = []
-            for x in range(0, amount):
-                value = data[from_col - 1].pop()
-                temp_list.append(value)
-            temp_list.reverse()
-            for y in temp_list:
-                data[to_col - 1].append(y)
+            data[to_col - 1].extend(data[from_col - 1][-amount:])
+            data[from_col - 1] = data[from_col - 1][:-amount]
             # print(data[to_col - 1])
 
     print(data)
-    if "test" in file:
-        print(f" final {data[0].pop()} {data[1].pop()} {data[2].pop()}")
-    else:
-        print(
-            f" final {data[0].pop()}{data[1].pop()}{data[2].pop()}{data[3].pop()}{data[4].pop()}{data[5].pop()}{data[6].pop()}{data[7].pop()}{data[8].pop()}")
+    top = [value[-1] for value in data]
+    print("".join(top))
 
 
 if __name__ == "__main__":
