c6f80c210248ca6b5e1a2118e8e640ac471fbc22
eballo <enric.ballo@gmail.com>
2023-02-21 19:45:37 +0100

    day01 - 2015

---
+++ 2015/day01/day01.py
@@ -0,0 +1,53 @@
+def load_file(file):
+    with open(file) as f:
+        values = [str(line) for line in f.readlines()]
+    return values
+
+
+def part_one(file: str):
+    raw_values = load_file(file)
+
+    print(raw_values)
+    for line in raw_values:
+        floor = 0
+
+        for x in range(0, len(line)):
+            if line[x] == '(':
+                floor = floor + 1
+            elif line[x] == ')':
+                floor = floor - 1
+
+        print(f"floor: {floor}")
+
+
+def part_two(file: str):
+    raw_values = load_file(file)
+
+    for line in raw_values:
+        enter_first_floor_pos = []
+        print(line)
+        floor = 0
+
+        for x in range(0, len(line)):
+            if line[x] == '(':
+                floor = floor + 1
+            elif line[x] == ')':
+                floor = floor - 1
+
+            # print(floor)
+            if floor == -1:
+                enter_first_floor_pos.append(x+1)
+                # print(f"position: {x+1}")
+        if enter_first_floor_pos:
+            print(f"postition: {enter_first_floor_pos[0]}")
+
+
+if __name__ == "__main__":
+    print("=== Part 1 Test ==")
+    part_one("test.txt")
+    print("=== Part 1 Input ==")
+    part_one("input.txt")
+    print("=== Part 2 Test ==")
+    part_two("test.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
