091313c7d67a268cca74436baf59aae15620d8ff
eballo <enric.ballo@gmail.com>
2023-11-19 11:47:57 +0100

    day02 - part2

--- 2020/day02/day02.py
+++ 2020/day02/day02.py
@@ -6,7 +6,6 @@ def load_file(file):
 
 
 def part_one(file: str):
-    # Find the two entries that sum to 2020; what do you get if you multiply them together?
     raw_values = load_file(file)
 
     total_valid = 0
@@ -19,7 +18,7 @@ def part_one(file: str):
         letter = columns[1].replace(":", "")
         password = columns[2]
         count = password.count(letter)
-        if count >= min and count <= max:
+        if min <= count <= max:
             print(f"{line} is valid")
             total_valid += 1
         else:
@@ -31,8 +30,25 @@ def part_one(file: str):
 
 def part_two(file: str):
     raw_values = load_file(file)
-    for value in range(len(raw_values)):
-        print(value)
+
+    total_valid = 0
+    total_invalid =0
+    for line in raw_values:
+        columns = line.split(" ")
+        min_max = columns[0].split("-")
+        min = int(min_max[0])
+        max = int(min_max[1])
+        letter = columns[1].replace(":", "")
+        password = columns[2]
+
+        if (password[min-1] == letter or password[max-1] == letter) and (password[min-1] != password[max-1]):
+            print(f"{line} is valid")
+            total_valid += 1
+        else:
+            print(f"{line} is not valid")
+            total_invalid += 1
+    print(f"Total valid passwords: {total_valid}")
+    print(f"Total invalid passwords: {total_invalid}")
 
 
 if __name__ == "__main__":
@@ -40,7 +56,7 @@ if __name__ == "__main__":
     part_one("test.txt")
     print("=== Part 1 Input ==")
     part_one("input.txt")
-    # print("=== Part 2 Test ==")
-    # part_two("test.txt")
-    # print("=== Part 2 Input ==")
-    # part_two("input.txt")
+    print("=== Part 2 Test ==")
+    part_two("test.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
