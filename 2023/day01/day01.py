9ef6b50a82b2e54a012ef2eabb8e018b0dca66ca
eballo <enric.ballo@gmail.com>
2023-12-01 08:31:38 +0100

    day01 - part 1

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -7,12 +7,24 @@ def load_file(file):
 
 def part_one(file: str):
     # Find the two entries that sum to 2020; what do you get if you multiply them together?
-    raw_values = load_file(file)
+    raw_lines = load_file(file)
 
-    for value in range(len(raw_values)):
-        print(value)
+    numbers = []
+    for line in raw_lines:
+        number = ""
+        for character in line:
+            if character.isdigit():
+                number += character
+            # print(character)
+        numbers.append(int(number[0] + number[-1]))
 
+    print(numbers)
+    total = 0
+    for number in numbers:
+        total += number
+    print(total)
 
+    
 def part_two(file: str):
     raw_values = load_file(file)
     for value in range(len(raw_values)):
@@ -22,8 +34,8 @@ def part_two(file: str):
 if __name__ == "__main__":
     print("=== Part 1 Test ==")
     part_one("test.txt")
-    # print("=== Part 1 Input ==")
-    # part_one("input.txt")
+    print("=== Part 1 Input ==")
+    part_one("input.txt")
     # print("=== Part 2 Test ==")
     # part_two("test.txt")
     # print("=== Part 2 Input ==")
