3e09ac746d325d83604dc9907768b85be10be384
eballo <enric.ballo@gmail.com>
2023-12-03 21:39:02 +0100

    day03 - part 2

--- 2023/day03/day03.py
+++ 2023/day03/day03.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def load_file(file):
     with open(file) as f:
         values = [line.rstrip() for line in f.readlines()]
@@ -11,13 +14,15 @@ moves = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 #  . . .  1
 #  . n .  0
 #  . . * -1
-def _has_adjacent_symbol(engine, r, c, nrows, ncols, has_adjacent_symbol):
+def _has_adjacent_symbol(engine, r, c, nrows, ncols, has_adjacent_symbol, adjacent_symbol_position=None) -> bool:
     for dr, dc in moves:
         nr, nc = r + dr, c + dc
         if 0 <= nr < nrows and 0 <= nc < ncols:
             character = engine[nr][nc]
             if not character.isdigit() and character != '.':
                 has_adjacent_symbol = True
+                if adjacent_symbol_position is not None:
+                    adjacent_symbol_position.append((nr, nc))
                 break
     return has_adjacent_symbol
 
@@ -31,6 +36,13 @@ def check_when_no_digit(number, has_adjacent_symbol, adjacents_numbers, not_adja
         else:
             # print(f" We have a number {number} without adjacent symbol")
             not_adjacents_numbers.append(int(number))
+
+
+def check_when_no_digit_adjacent(number, adjacent_symbol_position, has_adjacent_symbol, gear_ratio_candidates):
+    if len(number) > 0 and len(adjacent_symbol_position) > 0:
+        if has_adjacent_symbol:
+            for position in adjacent_symbol_position:
+                gear_ratio_candidates[position].append(int(number))
 
 
 def part_one(file: str):
@@ -62,18 +74,44 @@ def part_one(file: str):
 
 
 def part_two(file: str):
-    raw_values = load_file(file)
+    raw_lines = load_file(file)
 
-    for value in raw_values:
-        print(value)
+    engine = [[c for c in line] for line in raw_lines]
+    nrows, ncols = len(engine), len(engine[0])
+
+    # I will keep the gear ratio candidates in a dict
+    # gear_ratio_candidates = {(x, y): [numbers]}
+    gear_ratio_candidates = defaultdict(list)
+
+    for r in range(nrows):
+        number = ""
+        has_adjacent_symbol = False
+        adjacent_symbol_position = []
+        for c in range(ncols):
+            if engine[r][c].isdigit():
+                number += engine[r][c]
+                if adjacent_symbol_position:
+                    continue
+                has_adjacent_symbol = _has_adjacent_symbol(engine, r, c, nrows, ncols, has_adjacent_symbol, adjacent_symbol_position)
+            else:
+                check_when_no_digit_adjacent(number, adjacent_symbol_position, has_adjacent_symbol, gear_ratio_candidates)
+                has_adjacent_symbol = False
+                number = ""
+                adjacent_symbol_position = []
+
+        # edge case when the number finish at the end of the line
+        check_when_no_digit_adjacent(number, adjacent_symbol_position, has_adjacent_symbol, gear_ratio_candidates)
+
+    print(f"gear ratio candidates : {gear_ratio_candidates}")
+    print(f"Sum of gears ratio candidates : {sum(gears[0] * gears[1] for gears in gear_ratio_candidates.values() if len(gears) == 2)}")
 
 
 if __name__ == "__main__":
-    print("=== Part 1 Test ==")
-    part_one("test.txt")
-    print("=== Part 1 Input ==")
-    part_one("input.txt")
-    # print("=== Part 2 Test ==")
-    # part_two("test.txt")
-    # print("=== Part 2 Input ==")
-    # part_two("input.txt")
+    # print("=== Part 1 Test ==")
+    # part_one("test.txt")
+    # print("=== Part 1 Input ==")
+    # part_one("input.txt")
+    print("=== Part 2 Test ==")
+    part_two("test.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
