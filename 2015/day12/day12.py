de9c3901cd43cea12c6a6194a36f9a25d30f2f4b
Enric Ballo <enric.ballo@gmail.com>
2024-06-06 09:32:22 +0200

    day12 - 2015 - part2

--- 2015/day12/day12.py
+++ 2015/day12/day12.py
@@ -18,6 +18,18 @@ def calculate_sum_all_numbers(json_data: Any):
     return 0
 
 
+def calculate_sum_all_numbers_not_red(json_data: Any):
+    if isinstance(json_data, int):
+        return json_data
+    elif isinstance(json_data, list):
+        return sum(calculate_sum_all_numbers_not_red(item) for item in json_data)
+    elif isinstance(json_data, dict):
+        if "red" in json_data.values():
+            return 0
+        return sum(calculate_sum_all_numbers_not_red(value) for value in json_data.values())
+    return 0
+
+
 def part_one(file: str):
     raw_values = load_file(file)
 
@@ -28,6 +40,10 @@ def part_one(file: str):
 
 def part_two(file: str):
     raw_values = load_file(file)
+
+    json_data = json.loads(raw_values[0])
+    total_sum = calculate_sum_all_numbers_not_red(json_data)
+    print(f"Sum of all numbers: {total_sum}")
 
 
 if __name__ == "__main__":
