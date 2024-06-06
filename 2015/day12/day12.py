53bc22baf2dd46a3c940daf600b76626e95a41f0
Enric Ballo <enric.ballo@gmail.com>
2024-06-06 09:29:30 +0200

    day12 - 2015 - part1

---
+++ 2015/day12/day12.py
@@ -0,0 +1,38 @@
+import json
+from typing import Any
+
+
+def load_file(file):
+    with open(file) as f:
+        values = [line.rstrip() for line in f.readlines()]
+    return values
+
+
+def calculate_sum_all_numbers(json_data: Any):
+    if isinstance(json_data, int):
+        return json_data
+    elif isinstance(json_data, list):
+        return sum(calculate_sum_all_numbers(item) for item in json_data)
+    elif isinstance(json_data, dict):
+        return sum(calculate_sum_all_numbers(value) for value in json_data.values())
+    return 0
+
+
+def part_one(file: str):
+    raw_values = load_file(file)
+
+    json_data = json.loads(raw_values[0])
+    total_sum = calculate_sum_all_numbers(json_data)
+    print(f"Sum of all numbers: {total_sum}")
+
+
+def part_two(file: str):
+    raw_values = load_file(file)
+
+
+if __name__ == "__main__":
+    print("=== Part 1 Input ==")
+    part_one("input.txt")
+
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
