c3ad886aeaf4f5615551860200e7571061f1d9f6
Enric Ballo <enric.ballo@gmail.com>
2024-06-09 20:38:54 +0200

    day14 - 2015 - part2

--- 2015/day14/day14.py
+++ 2015/day14/day14.py
@@ -20,7 +20,12 @@ def parse_raw_data(raw_values: List[str]):
         reindeer_map[name] = {
             'speed': speed,
             'fly_time': fly_time,
-            'rest_time': rest_time
+            'rest_time': rest_time,
+            'distance': 0,
+            'points': 0,
+            'current_fly_time': 0,
+            'current_rest_time': 0,
+            'is_flying': True
         }
     return reindeer_map
 
@@ -47,11 +52,43 @@ def find_longest_distance(reindeer_map: dict, total_time: int) -> tuple[int, str
     return max_distance, reindeer_name
 
 
+def end_of_second_calculation(reindeer_map: dict):
+    for reindeer in reindeer_map.keys():
+        if reindeer_map[reindeer]['is_flying']:
+            reindeer_map[reindeer]['distance'] += reindeer_map[reindeer]['speed']
+            reindeer_map[reindeer]['current_fly_time'] += 1
+            if reindeer_map[reindeer]['current_fly_time'] == reindeer_map[reindeer]['fly_time']:
+                reindeer_map[reindeer]['is_flying'] = False
+                reindeer_map[reindeer]['current_fly_time'] = 0
+        else:
+            reindeer_map[reindeer]['current_rest_time'] += 1
+            if reindeer_map[reindeer]['current_rest_time'] == reindeer_map[reindeer]['rest_time']:
+                reindeer_map[reindeer]['is_flying'] = True
+                reindeer_map[reindeer]['current_rest_time'] = 0
+
+
+def award_points(reindeer_map: dict):
+    max_distance = max(reindeer_map[reindeer]['distance'] for reindeer in reindeer_map.keys())
+    for reindeer in reindeer_map.keys():
+        if reindeer_map[reindeer]['distance'] == max_distance:
+            reindeer_map[reindeer]['points'] += 1
+
+
+def find_max_points(reindeer_map: dict, total_time: int) -> tuple[int, str]:
+    for _ in range(total_time):
+        end_of_second_calculation(reindeer_map)
+        award_points(reindeer_map)
+
+    max_points = max(reindeer_map[reindeer]['points'] for reindeer in reindeer_map.keys())
+    reindeer_name = list(reindeer for reindeer in reindeer_map.keys() if reindeer_map[reindeer]['points'] == max_points )
+    return max_points, reindeer_name[0]
+
+
 def part_one(file: str):
     raw_values = load_file(file)
 
     reindeer_map = parse_raw_data(raw_values)
-    print(json.dumps(reindeer_map, indent=2))
+    # print(json.dumps(reindeer_map, indent=2))
 
     total_time = 2503
     max_distance, reinder_name = find_longest_distance(reindeer_map, total_time)
@@ -61,13 +98,17 @@ def part_one(file: str):
 def part_two(file: str):
     raw_values = load_file(file)
 
-    for value in raw_values:
-        print(value)
+    reindeer_map = parse_raw_data(raw_values)
+    # print(json.dumps(reindeer_map, indent=2))
+
+    total_time = 2503
+    max_points, reinder_name = find_max_points(reindeer_map, total_time)
+    print(f"how many points does the winning reindeer have?: {max_points} - {reinder_name}")
 
 
 if __name__ == "__main__":
     print("=== Part 1 Input ==")
     part_one("input.txt")
 
-    # print("=== Part 2 Input ==")
-    # part_two("input.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
