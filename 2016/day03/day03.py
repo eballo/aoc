3de9e6bff5a1c9eb0e4e2badff30b06bd8caf84c
Enric Ballo <enric.ballo@gmail.com>
2024-06-14 18:55:36 +0200

    day03 - 2016 - part2

--- 2016/day03/day03.py
+++ 2016/day03/day03.py
@@ -27,6 +27,22 @@ def calculate_how_many_are_valid(triangles: List[List[int]]) -> int:
     return valid
 
 
+def calculate_how_many_are_valid_by_column(triangles: List[List[int]]) -> int:
+    valid = 0
+    for i in range(0, len(triangles), 3):
+        group = triangles[i:i + 3]  # group the first 3 rows
+        # print(group)
+        # There should always be 3 lines in a group, or skip if less
+        if len(group) < 3:
+            continue
+        # Transpose the lines to get columns
+        for j in range(0, 3):
+            sides = [group[0][j], group[1][j], group[2][j]]
+            if is_valid_triangle(sides):
+                valid += 1
+    return valid
+
+
 def part_one(file: str):
     raw_values = load_file(file)
 
@@ -38,14 +54,15 @@ def part_one(file: str):
 
 def part_two(file: str):
     raw_values = load_file(file)
-
-    for value in raw_values:
-        print(value)
+    triangles = parse_input(raw_values)
+    # print(triangles)
+    total_valid = calculate_how_many_are_valid_by_column(triangles)
+    print(f"how many of the listed triangles are possible? {total_valid}")
 
 
 if __name__ == "__main__":
     print("=== Part 1 Input ==")
     part_one("input.txt")
 
-    # print("=== Part 2 Input ==")
-    # part_two("input.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
