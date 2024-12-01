80c78ede2f14ef78a336548a0683d38798eb2ce3
Enric Ballo <enric.ballo@gmail.com>
2024-12-01 12:00:38 +0100

    WIP day12 - 2022 - part2

--- 2022/day12/day12.py
+++ 2022/day12/day12.py
@@ -11,7 +11,7 @@ class Heightmap:
     def __init__(self, field: list[list[str]]) -> None:
         self.field = field
         self.visited_positions = set()
-        self.queue = deque()
+        self.queue = None
 
     def get_start_position(self) -> tuple[int,int]:
         return self._get_and_replace_position_for_letter("S", "a")
@@ -26,16 +26,24 @@ class Heightmap:
                     self.field[x][y] = replace_letter
                     return x,y
 
+    def find_distance_path_from_all_possible_start_positions(self, start_positions: list[tuple[int,int]], end_position:tuple[int,int]) -> list[int]:
+        distance_paths =[]
+        for position in start_positions:
+            new_distance = self.find_distance_path(position, end_position)
+            distance_paths.append(new_distance)
+        return distance_paths
+
+
     def find_distance_path(self, start_position: tuple[int,int], end_position: tuple[int, int]) -> int:
         self.visited_positions.add(start_position)
+        self.queue = deque()
         distance = 1
         self.queue.append((distance, start_position)) # distance, position
         finished = False
-        while queue or finished:
+        while self.queue or finished:
             if finished:
                 break
             distance, position = self.queue.popleft()
-            print(position)
             position = self.get_possible_paths(position)
             finished = self.found_position_or_queue(distance, position, end_position, finished)
 
@@ -63,7 +71,6 @@ class Heightmap:
             if(x_next, y_next) in self.visited_positions:
                 continue
             # check elevation and if the difference is grater than 1 continue
-            print(x_next, y_next)
             elevation_number_current = self.field[position[0]][position[1]]
             elevation_number_next = self.field[x_next][y_next]
             if LOWERCASE[elevation_number_next] - LOWERCASE[elevation_number_current] >1:
@@ -72,6 +79,17 @@ class Heightmap:
             # we have a valid node and was not visit
             positions.append((x_next, y_next))
         return positions
+
+    def get_all_start_possible_positions(self) -> list[tuple[int,int]]:
+        start_positions = []
+        for x, row in enumerate(self.field):
+            for y, column in enumerate(row):
+                if column == "S":
+                    self.field[x][y] = "a"
+                    start_positions.append((x, y))
+                if column == "a":
+                    start_positions.append((x, y))
+        return start_positions
 
 def get_raw_data(file: str) -> list[list[str]]:
     field =[]
@@ -98,14 +116,22 @@ def part_one(file: str):
 
 def part_two(file: str):
     raw_data = get_raw_data(file)
+    heightmap = Heightmap(raw_data)
 
-    for value in raw_data:
-        print(value)
+    start_positions = heightmap.get_all_start_possible_positions()
+    end_position = heightmap.get_end_position()
+    all_distances_paths = heightmap.find_distance_path_from_all_possible_start_positions(start_positions, end_position)
+
+    print(len(all_distances_paths))
+    all_distances_paths.sort()
+    print(set(all_distances_paths))
+    print(f"What is the fewest steps required to move from your current position to the location that should get the best signal? {all_distances_paths[0]}")
+
 
 
 if __name__ == "__main__":
     print("=== Part 1 Input ==")
     part_one("input.txt")
 
-    # print("=== Part 2 Input ==")
-    # part_two("input.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
