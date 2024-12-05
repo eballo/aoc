74a11dff2f22b492452f01eee64828fcdc6d209c
Enric Ballo <enric.ballo@gmail.com>
2024-12-05 08:28:55 +0100

    day04 - 2024 - part2

--- 2024/day04/day04.py
+++ 2024/day04/day04.py
@@ -6,25 +6,6 @@ def get_raw_data(file: str) -> list[str]:
         values = [line.rstrip() for line in f.readlines()]
     return values
 
-# ..X...
-# .SAMX.
-# .A..A.
-# XMAS.S
-# .X....
-
-def find_word(raw_data: list[str], position: tuple[int, int], direction: tuple[int, int], word:str, matrix_size:tuple[int, int]) -> bool:
-    dx, dy = direction
-    for i, letter in enumerate(word):
-        px = position[0] + i * dx
-        py = position[1] + i * dy
-        if not (0 <= px < matrix_size[0] and 0 <= py < matrix_size[1]):
-            return False
-        if raw_data[px][py] == letter:
-            print(f"{letter} found!")
-        if raw_data[px][py] != letter:
-            return False
-    return True
-
 
 def get_all_possible_directions_from_a_position() -> list[tuple[int, int]]:
     """
@@ -39,6 +20,44 @@ def get_all_possible_directions_from_a_position() -> list[tuple[int, int]]:
                 all_possible_directions.append((dx, dy))
     return all_possible_directions
 
+
+# ..X...
+# .SAMX.
+# .A..A.
+# XMAS.S
+# .X....
+
+def find_word(raw_data: list[str], position: tuple[int, int], direction: tuple[int, int], word:str, matrix_size:tuple[int, int]) -> bool:
+    dx, dy = direction
+    for i, letter in enumerate(word):
+        px = position[0] + i * dx
+        py = position[1] + i * dy
+        if not (0 <= px < matrix_size[0] and 0 <= py < matrix_size[1]):
+            return False
+        # if raw_data[px][py] == letter:
+        #     print(f"{letter} found!")
+        if raw_data[px][py] != letter:
+            return False
+    return True
+
+
+def find_mas(raw_data: list[str], position: tuple[int, int], matrix_size:tuple[int, int]) -> bool:
+    if not (1 <= position[0] < (matrix_size[0] - 1) and 1 <= position[1] < (matrix_size[1] - 1)):
+        return False
+    if raw_data[position[0]][position[1]] != "A":
+        return False
+
+    # Check diagonals
+    # -1 +1    +1 +1
+    #       A
+    # -1 -1    +1 -1
+    diagonal1 = f"{raw_data[position[0] - 1][position[1] - 1]}{raw_data[position[0] + 1][position[1] + 1]}"
+    diagonal2 = f"{raw_data[position[0] - 1][position[1] + 1]}{raw_data[position[0] + 1][position[1] - 1]}"
+    if diagonal1 in ["MS", "SM"] and diagonal2 in ["MS", "SM"]:
+            return True
+    return False
+
+
 def part_one(file: str):
     raw_data = get_raw_data(file)
 
@@ -49,11 +68,19 @@ def part_one(file: str):
     col_size = len(raw_data[0])
     matrix_size =(row_size, col_size)
 
+    print(f"matrix_size {matrix_size}")
+
     total = 0
     for row in range(row_size):
         for col in range(col_size):
             for direction in all_possible_directions:
-                total += find_word(raw_data,(row, col), direction, "XMAS", matrix_size)
+                total += find_word(
+                    raw_data,
+           (row, col),
+                    direction,
+                    "XMAS",
+                    matrix_size
+                )
 
     print(f"How many times does XMAS appear? {total}")
 
@@ -61,13 +88,23 @@ def part_one(file: str):
 def part_two(file: str):
     raw_data = get_raw_data(file)
 
-    for value in raw_data:
-        print(value)
+    row_size = len(raw_data)
+    col_size = len(raw_data[0])
+    matrix_size =(row_size, col_size)
+
+    print(f"matrix_size {matrix_size}")
+
+    total = 0
+    for row in range(row_size):
+        for col in range(col_size):
+            total += find_mas(raw_data,(row, col), matrix_size)
+
+    print(f"How many times does XMAS appear? {total}")
 
 
 if __name__ == "__main__":
     print("=== Part 1 Input ==")
     part_one("input.txt")
 
-    # print("=== Part 2 Input ==")
-    # part_two("input.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
