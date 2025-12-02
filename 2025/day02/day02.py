f365d560d71832e034c76e959f91bb6d5e7b12ea
Enric Ballo <enric.ballo@gmail.com>
2025-12-02 08:06:25 +0100

    day02 - 2025 - part2

--- 2025/day02/day02.py
+++ 2025/day02/day02.py
@@ -27,33 +27,72 @@ def is_repeated_sequence(number: int) -> bool:
     return first_half == second_half
 
 
+def is_periodic_id(number: int) -> bool:
+    """
+    Now, an ID is invalid if it is made only of some sequence of digits repeated at least twice.
+    Examples:
+      11 (1 repeated 2x) -> True
+      111 (1 repeated 3x) -> True
+      121212 (12 repeated 3x) -> True
+      123123 (123 repeated 2x) -> True
+      123123123 (123 repeated 3x) -> True
+      123124 (not a repetition) -> False
+    """
+    s = str(number)
+    length = len(s)
+
+    # Divide the string lenth by 2 and round down to get the max chunk size
+    for chunk_len in range(1, (length // 2) + 1):
+
+        # only consider chunk lengths that divide evenly into the string length
+        if length % chunk_len == 0:
+            # get the chunk pattern
+            pattern = s[:chunk_len]
+            # determine how many times it should repeat
+            multiplier = length // chunk_len
+
+            # Reconstruct what the string should look like
+            if pattern * multiplier == s:
+                return True
+    return False
+
+
 def part_one(file: str):
     raw_data = get_raw_data(file)
     list_of_values = raw_data[0].split(",")
     invalid_ids_found = []
     for value in list_of_values:
         first, last = value.split("-")
-        print(first, last)
+        # print(first, last)
         for num in range(int(first), int(last) + 1):
             if is_repeated_sequence(num):
                 invalid_ids_found.append(num)
 
     print(f"Found {len(invalid_ids_found)} invalid IDs.")
     print(
-        f"What do you get if you add up all of the invalid IDs?{sum(invalid_ids_found)}"
+        f"What do you get if you add up all of the invalid IDs? {sum(invalid_ids_found)}"
     )
 
 
 def part_two(file: str):
     raw_data = get_raw_data(file)
+    list_of_values = raw_data[0].split(",")
+    invalid_ids_found = []
+    for value in list_of_values:
+        first, last = value.split("-")
+        # print(first, last)
+        for num in range(int(first), int(last) + 1):
+            if is_periodic_id(num):
+                invalid_ids_found.append(num)
 
-    for value in raw_data:
-        print(value)
+    print(
+        f"What do you get if you add up all of the invalid IDs using these new rules? {sum(invalid_ids_found)}"
+    )
 
 
 if __name__ == "__main__":
     print("=== Part 1 Input ==")
     part_one("input.txt")
 
-    # print("=== Part 2 Input ==")
-    # part_two("input.txt")
+    print("=== Part 2 Input ==")
+    part_two("input.txt")
